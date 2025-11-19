80ed4bd1e55582bab8a60ce361dbb8c691d4458a
David Acosta <david.acosta.ing@gmail.com>
2025-11-18 19:20:44 -0500

    Particles when Enemy dies. Project clean up

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -1,16 +1,31 @@
+ using System;
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    public Action OnDeath;
     [SerializeField] private GameObject _splatterPrefab;
+    [SerializeField] private GameObject _deathParticlesPrefab ;
     [SerializeField] private int _startingHealth = 3;
 
     private int _currentHealth;
 
     private void Start() {
         ResetHealth();
+    }
+
+    private void OnEnable()
+    {
+        OnDeath += SpawnDeathVFX;
+        OnDeath += SpawnDeathSplatterPrefab;
+    }
+
+    private void OnDisable()
+    {
+        OnDeath -= SpawnDeathVFX;
+        OnDeath -= SpawnDeathSplatterPrefab;
     }
 
     public void ResetHealth() {
@@ -21,7 +36,7 @@ public class Health : MonoBehaviour
         _currentHealth -= amount;
 
         if (_currentHealth <= 0) {
-            SpawnDeathSplatterPrefab();
+            OnDeath?.Invoke();
             Destroy(gameObject);
         }
     }
@@ -31,5 +46,13 @@ public class Health : MonoBehaviour
         var splatterInstance = Instantiate(_splatterPrefab, transform.position, Quaternion.identity);
         var colorChanger = gameObject.GetComponent<ColorChanger>();
         splatterInstance.GetComponent<SpriteRenderer>().color = colorChanger.DefaultColor;
+    }
+
+    private void SpawnDeathVFX()
+    {
+        var particlesInstance = Instantiate(_deathParticlesPrefab, transform.position, Quaternion.identity);
+        var particles = particlesInstance.GetComponent<ParticleSystem>().main;
+        var colorChanger = gameObject.GetComponent<ColorChanger>();
+        particles.startColor = colorChanger.DefaultColor;
     }
 }
