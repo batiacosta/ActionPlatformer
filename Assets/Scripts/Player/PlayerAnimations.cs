485f6b2589e73cca4bd2fd4af49b673d83f8035e
David Acosta <david.acosta.ing@gmail.com>
2025-11-19 10:51:01 -0500

    Hat tilting to reinforce player movement visual feedback

--- Assets/Scripts/Player/PlayerAnimations.cs
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -1,13 +1,14 @@
  using System;
  using UnityEngine;
- using UnityEngine.Rendering;
 
  public class PlayerAnimations : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _moveDustParticles;
     [SerializeField] private float _tiltAngle = 20;
     [SerializeField] private float _tiltSpeed = 5f;
+    [SerializeField] private float _hatTiltingModifier = 5;
     [SerializeField] private Transform _characterSpriteTransform;
+    [SerializeField] private Transform _hatSpriteTransform;
 
     private void Update()
     {
@@ -47,5 +48,13 @@
             tiltingAngle
         );
         _characterSpriteTransform.rotation = Quaternion.Lerp(currentPlayerRotation, targetCharacterRotation, _tiltSpeed * Time.deltaTime);
+
+        var currentHatRotation = _hatSpriteTransform.rotation;
+        var targetHatRotation = Quaternion.Euler(
+                currentHatRotation.eulerAngles.x,
+                currentHatRotation.eulerAngles.y,
+                - tiltingAngle / _hatTiltingModifier
+            );
+        _hatSpriteTransform.rotation = Quaternion.Lerp(currentHatRotation, targetHatRotation, _tiltSpeed * _hatTiltingModifier * Time.deltaTime);
     }
 }
