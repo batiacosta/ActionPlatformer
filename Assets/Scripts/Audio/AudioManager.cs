86822735fb9a268a9b3d9668260f3fa41e385328
David Acosta <david.acosta.ing@gmail.com>
2025-12-23 11:09:00 -0500

    Audio manager improved, and Jump pitch is not randomized

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,8 +1,10 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] private float _masterVolume = 1;
     [SerializeField] private SoundSO _gunShoot;
     [SerializeField] private SoundSO _jumpSO;
 
@@ -18,20 +20,39 @@ public class AudioManager : MonoBehaviour
         PlayerController.OnJump -= OnJump;
     }
 
-    private void PlaySound(SoundSO soundSO)
+    private void SoundToPlay(SoundSO soundSO)
+    {
+        var clip = soundSO.Clip;
+        var pitch = soundSO.Pitch;
+        var volume = soundSO.Volume * _masterVolume;
+        var loop =  soundSO.Loop;
+        if (soundSO.RandomizePitch)
+        {
+            var randomPitchModifier = Random.Range(-soundSO.RandomPitchRangeModifier, soundSO.RandomPitchRangeModifier);
+            pitch += randomPitchModifier;
+        }
+        
+        PlaySound(clip, pitch, volume, loop);
+    }
+    private void PlaySound(AudioClip clip,  float pitch, float volume, bool loop)
     {
         var soundObject = new GameObject("Temp Audio Source");
         var audioSource = soundObject.AddComponent<AudioSource>();
-        audioSource.clip = soundSO.Clip;
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.loop = loop;
+        audioSource.pitch = pitch;
         audioSource.Play();
+        
+        if(!loop) Destroy(soundObject, clip.length);
     }
 
     private void GunOnShoot()
     {
-        PlaySound(_gunShoot);
+        SoundToPlay(_gunShoot);
     }
     private void OnJump()
     {
-        PlaySound(_jumpSO);
+        SoundToPlay(_jumpSO);
     }
 }
