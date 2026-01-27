f5bd26c30a463f2026795845a3028dc751a80964
David Acosta <david.acosta.ing@gmail.com>
2026-01-27 17:26:59 -0500

    Setting up grenade essential logic, and prefabs

---
+++ Assets/Scripts/Gun/Grenade.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    // Rig up Player Input to launch it with Right click
+    // Lob grenade and spin  it with RigidBody2D.AddTorque
+    // Beeb 3 times before detonation
+    // Create explosion
+    // Use Physics2D.OverlapCircleAll to damage all enemies it hits
+}
