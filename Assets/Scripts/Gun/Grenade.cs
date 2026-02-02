a10bb6a397072656e6c01963ada397f58fceaa4b
David Acosta <david.acosta.ing@gmail.com>
2026-02-02 11:16:35 -0500

    Grenade physics behavior

--- Assets/Scripts/Gun/Grenade.cs
+++ Assets/Scripts/Gun/Grenade.cs
@@ -1,4 +1,7 @@
+using System;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Grenade : MonoBehaviour
 {
@@ -7,4 +10,29 @@ public class Grenade : MonoBehaviour
     // Beeb 3 times before detonation
     // Create explosion
     // Use Physics2D.OverlapCircleAll to damage all enemies it hits
+
+    [SerializeField] private float _launchForce = 15f;
+    [SerializeField] private float _torqueAmount = 2f;
+
+    private Rigidbody2D _rigidbody;
+    private CinemachineImpulseSource _impulseSource;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    private void Start()
+    {
+        LaunchGrenade();
+    }
+
+    private void LaunchGrenade()
+    {
+        var mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        var directionToMouse = (mousePos - transform.position).normalized;
+        _rigidbody.AddForce(directionToMouse * _launchForce, ForceMode2D.Impulse);
+        _rigidbody.AddTorque(_torqueAmount, ForceMode2D.Impulse);
+    }
 }
