9d19a5d23cd72353d8f0f94fc25385ab432c0d22
David Acosta <david.acosta.ing@gmail.com>
2026-02-03 13:03:51 -0500

    Null check

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -56,7 +56,7 @@ public class PlayerController : MonoBehaviour
     private void OnDestroy()
     {
         var fade = FindFirstObjectByType<Fade>();
-        fade.FadeInOut();
+        fade?.FadeInOut();
     }
 
     public bool CheckIfGrounded()
